84b3c6aef17b81bc8375a6d598594a0cb631a9db
Aleksandr Gusev <goozef@mail.ru>
2019-11-30 22:08:36 +0700

    Created ios project

---
+++ ios-app/ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-app
+//
+//  Created by Aleksandr Gusev on 30/11/2019.
+//  Copyright © 2019 aleksandr.gusev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
