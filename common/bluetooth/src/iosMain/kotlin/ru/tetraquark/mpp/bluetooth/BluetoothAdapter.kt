937e23b7911b93ac05dac856a827e660305a3454
Aleksandr Gusev <goozef@mail.ru>
2019-11-30 23:26:43 +0700

    Fixed adapter class

--- common/bluetooth/src/iosMain/kotlin/ru/tetraquark/mpp/bluetooth/BluetoothAdapter.kt
+++ common/bluetooth/src/iosMain/kotlin/ru/tetraquark/mpp/bluetooth/BluetoothAdapter.kt
@@ -3,7 +3,7 @@ package ru.tetraquark.mpp.bluetooth
 actual class BluetoothAdapter(
     actual val uuid: String,
     actual val messagesBufferLength: Int = 1024
-) {}
+) {
 
     actual fun isAvailable(): Boolean {
         return true
@@ -22,9 +22,9 @@ actual class BluetoothAdapter(
     actual fun stopDeviceDiscovery() {
     }
 
-    actual fun getDeviceName(): String = androidBluetoothAdapter.name
+    actual fun getDeviceName(): String = ""
 
-    actual fun getDeviceAddress(): String = androidBluetoothAdapter.address
+    actual fun getDeviceAddress(): String = ""
 
     actual fun connectTo(bluetoothRemoteDevice: BluetoothRemoteDevice): BluetoothConnection {
         TODO("")
